2aca7728475627e8a8879884f192059dbead89c3
EyalHadad <eyalhadad1989@gmail>
2021-05-17 23:33:41 +0300

    set cwd and copy dicts

--- src/Predicting/predict_targets.py
+++ src/Predicting/predict_targets.py
@@ -5,18 +5,16 @@ import numpy as np
 import os
 
 
-
-
-def load_and_preprocessing(drug_id,is_target):
-    d_path = os.path.join(r"../../data",drug_id+".csv")
+def load_and_preprocessing(drug_id, is_target):
+    d_path = os.path.join(r"../../data", drug_id + ".csv")
     dicts_path = "../../raw_data/for_train/dicts"
     data = pd.read_csv(d_path, index_col=False)
     print("---Train data was loaded---\n")
-    lbe = load(open(os.path.join(dicts_path,"weight_scalar.pkl"), 'rb'))
+    lbe = load(open(os.path.join(dicts_path, "weight_scalar.pkl"), 'rb'))
     data['weight'] = lbe.transform(data['weight'].values.reshape(-1, 1))
     print("---Transformed drug weight feature---\n")
     print(data.info)
-    x = data.drop(['drugBank_id','gene','protein'], axis=1)
+    x = data.drop(['drugBank_id', 'gene', 'protein'], axis=1)
     x = np.asarray(x).astype('float32')
     print("---Finished load and preprocessing data---\n")
     if is_target:
@@ -40,10 +38,10 @@ def add_belongs_to_training(to_save, drug_target_id):
     return to_save
 
 
-def use_model(_x, to_predict, drug_target_id, classifier, specific_target_model = False):
+def use_model(_x, to_predict, drug_target_id, classifier, specific_target_model=False):
     print("Loading model")
     if specific_target_model:
-        m_path = os.path.join(r"../../output", "{0}_{1}_model.h5".format(drug_target_id,classifier))
+        m_path = os.path.join(r"../../output", "{0}_{1}_model.h5".format(drug_target_id, classifier))
     else:
         m_path = os.path.join(r"../../output", "%s_model.h5" % classifier)
     model = keras.models.load_model(m_path)
@@ -54,19 +52,15 @@ def use_model(_x, to_predict, drug_target_id, classifier, specific_target_model 
     to_save = pd.concat([to_predict, df_pred], axis=1)
     if _is_target:
         to_save = add_col_names(to_save)
-        to_save = add_belongs_to_training(to_save,drug_target_id)
+        to_save = add_belongs_to_training(to_save, drug_target_id)
 
-    to_save.sort_values(by=['prediction'],ascending=False,inplace=True)
+    to_save.sort_values(by=['prediction'], ascending=False, inplace=True)
     to_save.to_csv(os.path.join(r"../../output", drug_target_id + "_prediction.csv"), index=False)
 
 
 if __name__ == '__main__':
-    _is_target=True
+    _is_target = True
     target_names = ['ifng', 'kat5', 'tyms', 'dhfr', 'tf', 'pdcd1', 'a2m']
     for to_predict in target_names:
         _x, _ids = load_and_preprocessing(to_predict, _is_target)
         use_model(_x, _ids, to_predict, "4_reg", specific_target_model=True)
-
-
-
-    
--- src/data_downloader/download_targets.py
+++ src/data_downloader/download_targets.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import os
+import shutil
 from src.data_downloader.connection_object import DataConnector
 import itertools
 from collections import Counter
 
 
-def create_dict(f_path,c_name,dict_size):
+def create_dict(f_path, c_name, dict_size):
     data = pd.read_csv(f_path)
-    splited_data= [x.split(":") for x in data[c_name].astype(str).to_list()]
+    splited_data = [x.split(":") for x in data[c_name].astype(str).to_list()]
     flaten_data = list(itertools.chain(*splited_data))
     data_counter = Counter(flaten_data)
     percentage_tuples = [(i, data_counter[i] / len(flaten_data) * 100.0) for i in data_counter]
-    percentage_tuples.sort(key=lambda tup: tup[1],reverse = True)
+    percentage_tuples.sort(key=lambda tup: tup[1], reverse=True)
     dict_list = [(v[0], k) for k, v in enumerate(percentage_tuples[:dict_size - 1], start=1)]
-    dict_list.append(('no value',dict_size))
+    dict_list.append(('no value', dict_size))
     return pd.DataFrame(dict_list)
 
 
 def create_target_dicts(dir_path):
     print("---creating dictionaries---")
-    dict_dir = os.path.join(dir_path,'dicts')
+    dict_dir = os.path.join(dir_path, 'dicts')
     if not os.path.exists(dict_dir):
         print("dictionaries folder was created")
         os.makedirs(dict_dir)
@@ -37,12 +38,20 @@ def download_targets_data(is_train):
     dir_path = "../../raw_data/for_" + is_train
     dw = DataConnector(user="drugsmaster", password="pass2DRUGS!")
     dw.connect()
-    dw.get_table(schema="uniport",table="gene",dst_file_path=os.path.join(dir_path,"protein_gene.csv"),headers=['protein','gene'])
-    dw.get_table(schema="uniport",table="keywords",dst_file_path=os.path.join(dir_path,"protein_keywords.csv"),headers=['protein','keywords'])
-    dw.get_table(schema="uniport",table="taxonomic_lineage",dst_file_path=os.path.join(dir_path,"protein_taxon.csv"),headers=['protein','taxon'])
+    dw.get_table(schema="uniport", table="gene", dst_file_path=os.path.join(dir_path, "protein_gene.csv"),
+                 headers=['protein', 'gene'])
+    dw.get_table(schema="uniport", table="keywords", dst_file_path=os.path.join(dir_path, "protein_keywords.csv"),
+                 headers=['protein', 'keywords'])
+    dw.get_table(schema="uniport", table="taxonomic_lineage", dst_file_path=os.path.join(dir_path, "protein_taxon.csv"),
+                 headers=['protein', 'taxon'])
     dw.disconnect()
-    if is_train=="train":
+    if is_train == "train":
         create_target_dicts(dir_path)
+    else:
+        train_dict_path = os.path.abspath(r"../../raw_data/for_train/dicts")
+        test_dict_path = os.path.join(os.path.abspath(dir_path), 'dicts')
+        shutil.copy(os.path.join(train_dict_path, 'keywords_dict.csv'), test_dict_path)
+        shutil.copy(os.path.join(train_dict_path, 'taxon_dict.csv'), test_dict_path)
 
 
 if __name__ == '__main__':
